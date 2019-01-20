d345da4a3d8717a7147699d08b82d52608a59541
titianaMannu <titianamannucci@gmail.com>
2019-01-21 00:01:00 +0100

    prima bozza richiesta modifica

--- src/entity/RequestForModification.java
+++ src/entity/RequestForModification.java
@@ -1,6 +1,15 @@
 package entity;
 
+import entity.modification.Modification;
+
+import java.time.LocalDate;
+
 public class RequestForModification {
     private RequestStatus status;
-    private String senderName;
+    private String senderNickname;
+    private String receiverNickname;
+    private LocalDate dateOfSubmission;
+    private long contractId;
+    private Modification modification;
+
 }
--- src/entity/RequestStatus.java
+++ src/entity/RequestStatus.java
@@ -1,5 +1,28 @@
 package entity;
 
+/**
+ *
+ * PENDING: il Mittente ha inviato la richiesta che ancora non è stata visualizzata. È possibile modificare i parametri della richiesta
+ * EVALUATING: Il Destinatario sta visualizzando e valutando la risposta. Non è possibile modificare i parametri della richiesta.
+ * ACCEPTED: La richiesta è stata accetata. Il Mittente non ha visualizzato la risposta
+ * DECLINED: il Destinatario ha considerato e rifutato la proposta; il Mittente non ha ancora visionato la risposta
+ * EXPIRED: richiesta scaduta. Il mittente non ha visualizzato la notifica
+ * CLOSED: il Mitente ha visualizzato la risposta.
+ */
 public enum RequestStatus {
-    DECLINED, ACCEPTED, PENDING;
+    PENDING(0),
+    EVALUATING(1),
+    ACCEPTED(2),
+    DECLINED(3),
+    EXPIRED(4),
+    CLOSED(5);
+
+    private int current;
+    private RequestStatus(int status){
+        this.current = status;
+    }
+
+    public int getCurrentStatus(){
+       return this.current;
+    }
 }
