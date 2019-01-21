142c16b59153ab8a1a55dbf055a1ab2fa863781d
titianaMannu <titianamannucci@gmail.com>
2019-01-21 23:11:16 +0100

    riferimento alla modifica viene spostata all'interno di state perché è parte dello stato della proposta.

--- src/entity/request/State.java
+++ src/entity/request/State.java
@@ -1,15 +1,36 @@
 package entity.request;
 
 import Beans.Contract;
+import entity.modification.Modification;
 
 public abstract class State {
     //per il DB
     protected RequestStatus status;
-    //lo stato del contratto contribuisce a quello della proposta
+    //il contratto è parte dello stato della proposta
     protected Contract contract;
+    //la modifica è parte dello stato della proposta
+    protected Modification modification;
 
-    public State(Contract contract) {
-        this.contract = contract;
+    public State(Contract contract, Modification modfc) throws IllegalArgumentException{
+        setContract(contract);
+        setModification(modfc);
+    }
+
+    protected void setModification(Modification modfc) throws IllegalArgumentException{
+        if (modfc== null) throw new IllegalArgumentException();
+        Contract tempC = modfc.update(contract);
+        if (!tempC.equals(contract)){
+            //confronto con equals:
+            this.modification = modfc;
+        }
+        else //modifica non ha effetti sul contratto
+            throw new IllegalArgumentException();
+    }
+
+
+    protected void setContract(Contract c)throws IllegalArgumentException{
+        if ( c == null) throw new IllegalArgumentException();
+        this.contract = c;
     }
 
     public RequestStatus getStatus() {
@@ -21,5 +42,5 @@ public abstract class State {
     }
 
     public abstract State forward(RequestForModification requestForModification);
-    public abstract State stop(RequestForModification requestForModification);
+    public abstract State decline(RequestForModification requestForModification);
 }
